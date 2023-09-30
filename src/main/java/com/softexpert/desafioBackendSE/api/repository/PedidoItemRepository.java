fcdd12e97d4c11b19e2c7d27c27e50d66466592d
Marcio <marciobarros@fiorde.com.br>
2023-09-30 09:23:02 -0300

    Remover classe necessarily

--- src/main/java/com/softexpert/desafioBackendSE/api/repository/PedidoItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.softexpert.desafioBackendSE.api.repository;
-
-import com.softexpert.desafioBackendSE.api.model.PedidoItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoItemRepository extends JpaRepository<PedidoItem, Integer> {
-}
